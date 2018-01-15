b3db5c465a7ff1f119887847d2e5f67891963c3a
Griffin <griffin@Griffins-Air.attlocal.net>
2018-01-14 19:23:24 -0600

    removed rogue apostrophe in view

--- scripts/view.js
+++ scripts/view.js
@@ -71,7 +71,7 @@ module.exports.printAttractionsByTime = (arr)=>{
                 if(attraction.times){attractionTimes = attraction.times.join(', ');}                // console.log(attractionTimes);
                      $('#descriptionArea').append(`
                         <div class='attraction item${area.id}' id='${attraction.id}'>
-                       <p><b> ${attraction.name}</b> - <span 'style='color:#${area.colorTheme}'>${area.name}</span></p>
+                       <p><b> ${attraction.name}</b> - <span style='color:#${area.colorTheme}'>${area.name}</span></p>
                         <p class='attrDescription' style='display:none'>
                         ${attraction.description}` + (attraction.times? `<br><br> <b>Start Times: ` + attractionTimes + `</b>`: '') + `
                         
